695f75c9ee13cd23de04673f808a5375f3c10913
Sudarshan Acharya <info@sacharya.com>
2014-08-08 05:52:14 +0000

    Typo on roles and cleanup of build script

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash 
 
 # ./build.sh --user root --ask-pass
-cd ~
-source env/bin/activate
-cd ansible-config
-#ansible-playbook -i rax.py build.yml $@ 2>&1
-
+source ~/env/bin/activate
 ansible-playbook -i inventory/hosts build.yml $@ 2>&1
-
-#ansible -i inventory/hosts openstackci -u root -m command -a "nodetool status"
